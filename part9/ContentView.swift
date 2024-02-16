b7f7e77a3b605e95f63be412cf01ccf2aa96e972
tomohiro1205 <tomohiro_1205@yahoo.co.jp>
2024-02-16 11:21:36 +0900

    Initial Commit

---
+++ part9/ContentView.swift
@@ -0,0 +1,54 @@
+//
+//  ContentView.swift
+//  part9
+//
+//  Created by 木村朋広 on 2024/02/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var isShowAlert = false
+    @State var isShowDailog = false
+
+    var body: some View {
+        VStack {
+            Button("アラート") {
+                isShowAlert = true
+            }
+            .padding()
+            Button ("ダイアログ") {
+                isShowDailog = true
+            }
+        }
+        .padding()
+        .alert("本当に削除しますか？", isPresented: $isShowAlert) {
+            Button("削除する", role: .destructive) {
+            }
+            Button("キャンセル", role: .cancel) {  }
+        } message: {
+            Text("一度削除したら元に戻せません。")
+        }
+        .confirmationDialog("タイトル", isPresented: $isShowDailog, titleVisibility: .visible) {
+            Button("選択肢1") {
+
+            } 
+            Button("選択肢2") {
+
+            }
+            Button("選択肢3") {
+
+            }
+            Button("選択肢4") {
+
+            }
+            Button("キャンセル", role: .cancel) {
+
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ part9/part9App.swift
@@ -0,0 +1,17 @@
+//
+//  part9App.swift
+//  part9
+//
+//  Created by 木村朋広 on 2024/02/16.
+//
+
+import SwiftUI
+
+@main
+struct part9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
